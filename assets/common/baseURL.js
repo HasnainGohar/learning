611cf55c401f805e9b5870ae7601419a3bbc4b3a
HasnainGohar823 <93980368+HasnainGohar@users.noreply.github.com>
2024-07-19 12:52:21 +0500

    assets

---
+++ assets/common/baseURL.js
@@ -0,0 +1,10 @@
+const baseURL = 'https://app.auditus.com/AusztechAttendanceApi/'
+
+// const baseURL = 'https://staging.ausz-tech.com/AusztechAttendanceApi/'
+
+// const baseURL = 'http://192.168.10.8:7182/' 
+
+
+
+
+export default baseURL
