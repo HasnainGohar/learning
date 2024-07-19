eeeae2ade6b54098b4f9fb648de7cbb107901335
HasnainGohar823 <93980368+HasnainGohar@users.noreply.github.com>
2024-07-19 12:52:58 +0500

    Update baseURL.js

--- assets/common/baseURL.js
+++ assets/common/baseURL.js
@@ -1,10 +1,3 @@
-const baseURL = 'https://app.auditus.com/AusztechAttendanceApi/'
-
-// const baseURL = 'https://staging.ausz-tech.com/AusztechAttendanceApi/'
-
 // const baseURL = 'http://192.168.10.8:7182/' 
-
-
-
 
 export default baseURL
